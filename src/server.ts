bf2adb8ceb5d22b642481267cb5dda3a6b32870e
Marcelo Dias <marcelodkarmo@gmail.com>
2021-08-11 20:44:29 -0300

    Aula02

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
+import "reflect-metadata"
 import express from "express"
+import { router } from "./routes"
 
+import "./database"
+
+const port = 3000
 const app = express()
 
-app.get('/test', (req, res) => {
-    return res.send("Olá NLW")
-})
+app.use(express.json())
+app.use(router)
 
-app.post('/test-post', (req, res) => {
-    return res.send("POST funcionou")
-})
+app.get("/test", (req, res) => {
+    res.send("Ta funfando")
+} )
 
-app.listen(3000, () => console.log("Server is running"))
+app.listen(port, () => console.log(`Server is running on port ${port}`))
